c8f939ab9679a777f42eefdac4e2b725e326300c
Onno Visser <visser.onno@gmail.com>
2019-03-09 12:21:48 +0100

    Test errors

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -4,5 +4,42 @@ const path = require('path');
 
 pluginTester({
   plugin,
+  babelOptions: { filename: __filename },
   fixtures: path.join(__dirname, 'fixtures'),
+  tests: [
+    {
+      code: `
+        import glsl from 'glslify';
+
+        glsl\`
+        #define ALPHA \${alpha}
+        \`;
+      `,
+      error: /non-initialized identifier/,
+    },
+    {
+      code: `
+        import glsl from 'glslify';
+
+        let alpha = 1;
+        alpha = .5
+
+        glsl\`
+        #define ALPHA \${alpha}
+        \`;
+      `,
+      error: /not constant/,
+    },
+    {
+      code: `
+        import glsl from 'glslify';
+        import { FOO } from './fixtures/constantImported/constants';
+
+        glsl\`
+        #define FOO \${FOO}
+        \`;
+      `,
+      error: /not exported/,
+    },
+  ],
 });
