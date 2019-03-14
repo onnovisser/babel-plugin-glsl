a3883f6ac5b55114ffdd2c351903a2fb6d5dd155
Onno Visser <visser.onno@gmail.com>
2019-03-14 19:58:24 +0100

    Allow accessing global object

--- test/fixtures/dynamic/code.js
+++ test/fixtures/dynamic/code.js
@@ -3,6 +3,7 @@ import glsl from 'glslify';
 function createShader({ alpha }) {
   return glsl`
     #define ALPHA ${alpha}
+    #define FOO ${globalThis.foo}
     void main () {
       gl_FragColor = vec4(1, 0, 0, ALPHA);
     }
--- test/fixtures/dynamic/output.js
+++ test/fixtures/dynamic/output.js
@@ -5,6 +5,8 @@ function createShader({
 
     #define ALPHA ${alpha}
 
+    #define FOO ${globalThis.foo}
+
     void main () {
       gl_FragColor = vec4(1, 0, 0, ALPHA);
     }
