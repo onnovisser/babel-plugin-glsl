1bdb68dc1a23e6f29b7fef074fdcf60ddea87064
Mohammed Khatib <mkhatib727@gmail.com>
2021-04-10 23:17:01 -0700

    Support calling expression for macros to allow glslify(path) evaluation

--- macro.js
+++ macro.js
@@ -1,12 +1,16 @@
 const { createMacro, MacroError } = require('babel-plugin-macros');
 const processGlslTag = require('./lib/processGlslTag');
 
-function glslifyMacro({ references, state }) {
+function glslifyMacro({ references, state, babel }) {
+  const t = babel.types;
   const { default: defaultImport = [] } = references;
 
   defaultImport.forEach(referencePath => {
     const path = referencePath.parentPath;
-    if (path.type === 'TaggedTemplateExpression') {
+    if (
+      (t.isCallExpression(path.node) &&
+        t.isStringLiteral(path.node.arguments[0])) ||
+      t.isTaggedTemplateExpression(path.node)) {
       try {
         processGlslTag(path, state);
       } catch (e) {
---
+++ test/macroFixtures/externalFile/myshader.frag
@@ -0,0 +1,4 @@
+#pragma glslify: import(./defines)
+void main () {
+  gl_FragColor = vec4(#ff0000, 1);
+}
